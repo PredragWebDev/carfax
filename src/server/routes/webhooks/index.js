454a8995f75387dcfd4138ea217094c8df9b9e2a
PredragWebDev <pedjolinodev@gmail.com>
2023-06-24 12:19:46 +0000

    modify

--- src/server/routes/webhooks/index.js
+++ src/server/routes/webhooks/index.js
@@ -127,158 +127,161 @@ const { sendErrorEmail, sendEmail } = require("../../classes/email");
         
             // Loop through all custom fields and get the VIN field
             // Then fetch the CARFAX report for that VIN and return the URL
-            if (VIN !== "") {
-          
-                console.log(`Received single report callback for VIN: ${VIN}`);
-          
-                // Get CARFAX report for the VIN from this order
-                const {
-                  Carfax,
-                  desktopReportHtml,
-                  yearMakeModel,
-                  error,
-                } = await getCarFax(VIN);
-          
-                if (error) {
-                  // If user has an account with the purchasing email then add a credit
-        
-                  const user = await User.findOne({ email:customer_email });
-          
-                  if (user)
-                    await user.updateOne({
-                      "subscription_data.balance":
-                        Number(user.subscription_data.balance) + 1,
-                    });
-          
-                  log({
-                    status: "error",
-                    type: "Selly Webhook: Purchased Single Report",
-                    data: JSON.stringify(
-                      { VIN, error, report: { Carfax: Carfax || null } },
-                      null,
-                      2
-                    ),
-                    user: null,
-                  });
-          
-                  await sendErrorEmail(customer_email);
-          
-                  res.send(error);
-                } else {
-                  // Create the report
-                  const report = await new Report({
-                    VIN,
-                    yearMakeModel,
+            if (VIN !== 'vin' && VIN !== 'VIN') {
+              if (VIN !== "") {
+            
+                  console.log(`Received single report callback for VIN: ${VIN}`);
+            
+                  // Get CARFAX report for the VIN from this order
+                  const {
+                    Carfax,
                     desktopReportHtml,
-                  });
+                    yearMakeModel,
+                    error,
+                  } = await getCarFax(VIN);
+            
+                  if (error) {
+                    // If user has an account with the purchasing email then add a credit
           
-                  // Save into DB
-                  await report.save();
-          
-                  // Check if the user already has an account
-                  const user = await User.findOne({ email:customer_email });
-          
-                  // If the user exists add the report to the users dashboard
-                  if (user) {
-                    await User.findOneAndUpdate(
-                      { _id: user._id },
-                      {
-                        $addToSet: { reports: report._id },
-                      }
+                    const user = await User.findOne({ email:customer_email });
+            
+                    if (user)
+                      await user.updateOne({
+                        "subscription_data.balance":
+                          Number(user.subscription_data.balance) + 1,
+                      });
+            
+                    log({
+                      status: "error",
+                      type: "Selly Webhook: Purchased Single Report",
+                      data: JSON.stringify(
+                        { VIN, error, report: { Carfax: Carfax || null } },
+                        null,
+                        2
+                      ),
+                      user: null,
+                    });
+            
+                    await sendErrorEmail(customer_email);
+            
+                    res.send(error);
+                  } else {
+                    // Create the report
+                    const report = await new Report({
+                      VIN,
+                      yearMakeModel,
+                      desktopReportHtml,
+                    });
+            
+                    // Save into DB
+                    await report.save();
+            
+                    // Check if the user already has an account
+                    const user = await User.findOne({ email:customer_email });
+            
+                    // If the user exists add the report to the users dashboard
+                    if (user) {
+                      await User.findOneAndUpdate(
+                        { _id: user._id },
+                        {
+                          $addToSet: { reports: report._id },
+                        }
+                      );
+                    }
+            
+                    log({
+                      status: "info",
+                      type: `Selly Webhook: Purchased Single Report`,
+                      data: JSON.stringify(
+                        {
+                          customer_email,
+                          VIN,
+                          report: {
+                            ID: report.id.toUpperCase(),
+                            yearMakeModel,
+                          },
+                        },
+                        null,
+                        2
+                      ),
+                      user: user ? user._id : customer_email,
+                    });
+            
+                    // Send email to make sure user gets report
+                    await sendEmail({
+                      to: customer_email,
+                      // templateId: "d-3c3876140e6149aca89f280e53163ec6",
+                      templateId: "1",
+                      dynamicTemplateData: {
+                        VIN:VIN,
+                        reportUrl: `${
+                          process.env.APP_URL
+                        }/report/${report.id.toUpperCase()}`,
+                      },
+                    });
+           
+                    // Return the URL
+                    return res.send(
+                      `${process.env.APP_URL}/report/${report.id.toUpperCase()}`
                     );
                   }
-          
-                  log({
-                    status: "info",
-                    type: `Selly Webhook: Purchased Single Report`,
-                    data: JSON.stringify(
-                      {
-                        customer_email,
-                        VIN,
-                        report: {
-                          ID: report.id.toUpperCase(),
-                          yearMakeModel,
-                        },
-                      },
-                      null,
-                      2
-                    ),
-                    user: user ? user._id : customer_email,
-                  });
-          
-                  // Send email to make sure user gets report
-                  await sendEmail({
-                    to: customer_email,
-                    // templateId: "d-3c3876140e6149aca89f280e53163ec6",
-                    templateId: "1",
-                    dynamicTemplateData: {
-                      VIN:VIN,
-                      reportUrl: `${
-                        process.env.APP_URL
-                      }/report/${report.id.toUpperCase()}`,
+              }
+              // User ordered reports or unlimited instead of a single VIN
+              else {
+                const user = await User.findOne({ email:customer_email });
+            
+                if (user) {
+                  let date = new Date();
+                  date.setDate(date.getDate() + 30);
+            
+                  const productTitle = String(product_title).toLowerCase();
+                  let balance = 0;
+            
+                  // Unlimited reports so lets add hella
+                  if (productTitle.includes("Unlimited".toLowerCase())) {
+                    balance = 99999;
+                    //console.log(`Received callback for Unlimited Reports for user ${email}`);
+                  }
+                  // Parse the amount of reports from the title
+                  else {
+                    balance = Number(productTitle.split(" Report".toLowerCase())[0]);
+                    //console.log(`Received callback for ${balance} Reports for user ${email}`);
+                  }
+            
+                  await user.updateOne({
+                    subscription_data: {
+                      balance: Number(user.subscription_data.balance) + balance,
+                      current_period_end: date,
+                      active: true,
                     },
                   });
-         
-                  // Return the URL
-                  return res.send(
-                    `${process.env.APP_URL}/report/${report.id.toUpperCase()}`
-                  );
+            
+                  log({
+                    status: "info",
+                    type: `Selly Webhook: Purchased ${
+                      balance === 99999 ? "Unlimited" : balance
+                    } Reports`,
+                    data: JSON.stringify({ customer_email }, null, 2),
+                    user: user._id,
+                  });
+            
+                  // Success email
+                  sendEmail({
+                    "to": customer_email,
+                    "templateId": "d-3c3876140e6149aca89f280e53163ec6",
+                  });
+                  res.send("Successfully upgraded your account!");
                 }
-            }
-            // User ordered reports or unlimited instead of a single VIN
-            else {
-              const user = await User.findOne({ email:customer_email });
-          
-              if (user) {
-                let date = new Date();
-                date.setDate(date.getDate() + 30);
-          
-                const productTitle = String(product_title).toLowerCase();
-                let balance = 0;
-          
-                // Unlimited reports so lets add hella
-                if (productTitle.includes("Unlimited".toLowerCase())) {
-                  balance = 99999;
-                  //console.log(`Received callback for Unlimited Reports for user ${email}`);
-                }
-                // Parse the amount of reports from the title
+                // Retard didn't make an account like supposed to
                 else {
-                  balance = Number(productTitle.split(" Report".toLowerCase())[0]);
-                  //console.log(`Received callback for ${balance} Reports for user ${email}`);
+                  sendEmail({
+                    "to": customer_email,
+                    "templateId": "d-3c3876140e6149aca89f280e53163ec6",
+                  });
+                  res.send("Email does not exist, please contact support!");
                 }
-          
-                await user.updateOne({
-                  subscription_data: {
-                    balance: Number(user.subscription_data.balance) + balance,
-                    current_period_end: date,
-                    active: true,
-                  },
-                });
-          
-                log({
-                  status: "info",
-                  type: `Selly Webhook: Purchased ${
-                    balance === 99999 ? "Unlimited" : balance
-                  } Reports`,
-                  data: JSON.stringify({ customer_email }, null, 2),
-                  user: user._id,
-                });
-          
-                // Success email
-                sendEmail({
-                  "to": customer_email,
-                  "templateId": "d-3c3876140e6149aca89f280e53163ec6",
-                });
-                res.send("Successfully upgraded your account!");
               }
-              // Retard didn't make an account like supposed to
-              else {
-                sendEmail({
-                  "to": customer_email,
-                  "templateId": "d-3c3876140e6149aca89f280e53163ec6",
-                });
-                res.send("Email does not exist, please contact support!");
-              }
+
             }
           }
         }
