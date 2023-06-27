1c64e14084d8c61f52417a2fcfc7a3fb3c5c65d9
PredragWebDev <pedjolinodev@gmail.com>
2023-06-27 07:09:34 +0000

    modify

--- src/server/routes/webhooks/index.js
+++ src/server/routes/webhooks/index.js
@@ -129,115 +129,108 @@ const { sendErrorEmail, sendEmail } = require("../../classes/email");
           
               // Loop through all custom fields and get the VIN field
               // Then fetch the CARFAX report for that VIN and return the URL
-              if (VIN !== 'vin' && VIN !== 'VIN' && VIN !== 'test') {
-                if (VIN !== "") {
-              
-                    console.log(`Received single report callback for VIN: ${VIN}`);
-              
-                    // Get CARFAX report for the VIN from this order
-                    const {
-                      Carfax,
-                      desktopReportHtml,
-                      yearMakeModel,
-                      error,
-                    } = await getCarFax(VIN);
-              
-                    if (error) {
-                      // If user has an account with the purchasing email then add a credit
-            
-                      const user = await User.findOne({ email:customer_email });
-              
-                      if (user)
-                        await user.updateOne({
-                          "subscription_data.balance":
-                            Number(user.subscription_data.balance) + 1,
-                        });
-              
-                      log({
-                        status: "error",
-                        type: "Selly Webhook: Purchased Single Report",
-                        data: JSON.stringify(
-                          { VIN, error, report: { Carfax: Carfax || null } },
-                          null,
-                          2
-                        ),
-                        user: null,
-                      });
-              
-                      await sendErrorEmail(customer_email);
+              if (VIN.length === 17 && !isValidVin(VIN)) {
+              // if (VIN !== 'vin' && VIN !== 'VIN' && VIN !== 'test') {
+                console.log(`Received single report callback for VIN: ${VIN}`);
+          
+                // Get CARFAX report for the VIN from this order
+                const {
+                  Carfax,
+                  desktopReportHtml,
+                  yearMakeModel,
+                  error,
+                } = await getCarFax(VIN);
+          
+                if (error) {
+                  // If user has an account with the purchasing email then add a credit
+        
+                  const user = await User.findOne({ email:customer_email });
+          
+                  if (user)
+                    await user.updateOne({
+                      "subscription_data.balance":
+                        Number(user.subscription_data.balance) + 1,
+                    });
+          
+                  log({
+                    status: "error",
+                    type: "Selly Webhook: Purchased Single Report",
+                    data: JSON.stringify(
+                      { VIN, error, report: { Carfax: Carfax || null } },
+                      null,
+                      2
+                    ),
+                    user: null,
+                  });
+          
+                  await sendErrorEmail(customer_email);
 
-                      const newWebhook = new Webhooks({webhooksID:req.body.webhookID, user:customer_email});
-                      await newWebhook.save();
-                      res.status(200).send('Webhook received successfully');
-              
-                      res.send(error);
-                    } else {
-                      // Create the report
-                      const report = await new Report({
-                        VIN,
-                        yearMakeModel,
-                        desktopReportHtml,
-                      });
-              
-                      // Save into DB
-                      await report.save();
-              
-                      // Check if the user already has an account
-                      const user = await User.findOne({ email:customer_email });
-              
-                      // If the user exists add the report to the users dashboard
-                      if (user) {
-                        await User.findOneAndUpdate(
-                          { _id: user._id },
-                          {
-                            $addToSet: { reports: report._id },
-                          }
-                        );
+                  const newWebhook = new Webhooks({webhooksID:req.body.webhookID, user:customer_email});
+                  await newWebhook.save();
+                  res.status(200).send('Webhook received successfully');
+          
+                  res.send(error);
+                } else {
+                  // Create the report
+                  const report = await new Report({
+                    VIN,
+                    yearMakeModel,
+                    desktopReportHtml,
+                  });
+          
+                  // Save into DB
+                  await report.save();
+          
+                  // Check if the user already has an account
+                  const user = await User.findOne({ email:customer_email });
+          
+                  // If the user exists add the report to the users dashboard
+                  if (user) {
+                    await User.findOneAndUpdate(
+                      { _id: user._id },
+                      {
+                        $addToSet: { reports: report._id },
                       }
-              
-                      log({
-                        status: "info",
-                        type: `Selly Webhook: Purchased Single Report`,
-                        data: JSON.stringify(
-                          {
-                            customer_email,
-                            VIN,
-                            report: {
-                              ID: report.id.toUpperCase(),
-                              yearMakeModel,
-                            },
-                          },
-                          null,
-                          2
-                        ),
-                        user: user ? user._id : customer_email,
-                      });
-              
-                      // Send email to make sure user gets report
-                      await sendEmail({
-                        to: customer_email,
-                        // templateId: "d-3c3876140e6149aca89f280e53163ec6",
-                        templateId: 1,
-                        subject:"Your order has been received",
-                        params: {
-                          VIN:VIN,
-                          reportUrl: `${
-                            process.env.APP_URL
-                          }/report/${report.id.toUpperCase()}`,
+                    );
+                  }
+          
+                  log({
+                    status: "info",
+                    type: `Selly Webhook: Purchased Single Report`,
+                    data: JSON.stringify(
+                      {
+                        customer_email,
+                        VIN,
+                        report: {
+                          ID: report.id.toUpperCase(),
+                          yearMakeModel,
                         },
-                        headers: {
-                          'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
-                        }
-                      });
-                      const newWebhook = new Webhooks({webhooksID:req.body.webhookId, user:customer_email});
-                      await newWebhook.save();
-                      
-                      // res.status(200).send('Webhook received successfully');
-                      // Return the URL
-                      // return res.send(
-                      //   `${process.env.APP_URL}/report/${report.id.toUpperCase()}`
-                      // );
+                      },
+                      null,
+                      2
+                    ),
+                    user: user ? user._id : customer_email,
+                  });
+          
+                  // Send email to make sure user gets report
+                  await sendEmail({
+                    to: customer_email,
+                    // templateId: "d-3c3876140e6149aca89f280e53163ec6",
+                    templateId: 1,
+                    subject:"Your order has been received",
+                    params: {
+                      VIN:VIN,
+                      reportUrl: `${
+                        process.env.APP_URL
+                      }/report/${report.id.toUpperCase()}`,
+                    },
+                    headers: {
+                      'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
                     }
+                  });
+                  const newWebhook = new Webhooks({webhooksID:req.body.webhookId, user:customer_email});
+                  await newWebhook.save();
+                  
                 }
                 // User ordered reports or unlimited instead of a single VIN
                 // else {
@@ -304,5 +297,31 @@ const { sendErrorEmail, sendEmail } = require("../../classes/email");
       res.status(200).send('Webhook received successfully');
     
   });
+
+  const isValidVin = (vin) => {
+    // Check that VIN contains only valid characters
+    var validChars = /^[A-HJ-NPR-Z0-9]*$/;
+    if (!validChars.test(vin)) {
+      return false;
+    }
+    
+    // Check that VIN follows correct format
+    var weights = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2];
+    var transliteration = { 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'J': 1, 'K': 2, 'L': 3, 'M': 4, 'N': 5, 'P': 7, 'R': 9, 'S': 2, 'T': 3, 'U': 4, 'V': 5, 'W': 6, 'X': 7, 'Y': 8, 'Z': 9 };
+    var sum = 0;
+    for (var i = 0; i < vin.length; i++) {
+      var charValue = transliteration[vin[i]] || parseInt(vin[i]);
+      if (isNaN(charValue)) {
+        return false;
+      }
+      sum += charValue * weights[i];
+    }
+    var checkDigit = sum % 11;
+    if (checkDigit === 10) {
+      return vin[8] === 'X';
+    } else {
+      return vin[8] == checkDigit;
+    }
+  }
 
 module.exports = router;
